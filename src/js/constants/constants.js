b552f56eec508b983bfcf2c04bf7213f711d26b2
Artem Nazarchenko <tiymanazar62@gmail.com>
2023-12-17 03:09:06 +0200

    Created simple function which is the first mode

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -2,7 +2,7 @@ export const modeListElem = document.querySelector('.modes__list');
 
 export const modes =  [
     {
-        name : "firstMode",
+        name : "Silly Spring",
         bgImage : "https://i.pinimg.com/564x/16/d3/71/16d371ddf4f4db8ca0e0c5ce5c9ff4ce.jpg"
     },
     {
@@ -12,14 +12,25 @@ export const modes =  [
     {
         name : "thirdMode",
         bgImage : "https://i.vimeocdn.com/video/665321658-770ecc7e16eb218c49ac762ab10d6d3c7ad4b7a87bb00ae7713ac8b141192910-d_640x360.jpg"
-    },
-    {
-        name : "firstMode",
-        bgImage : "https://i.pinimg.com/564x/16/d3/71/16d371ddf4f4db8ca0e0c5ce5c9ff4ce.jpg"
-    },
-    {
-        name : "secondMode",
-        bgImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmPnG0hZWB5-RFUOvvZqp9zcxy_UuyWamjuw&usqp=CAU"
-    },
-    
+    }
 ];
+
+export const settings = {
+    sillySpring : {
+        radius : {
+            name : 'Radius', 
+            type : 'range',
+            defaultValue : 50,
+        },
+        lineWidth : {
+            name : 'Line Width',
+            type : 'range',
+            defaultValue : 2,
+        },
+        color : {
+            type : 'select',
+            defaultValue : 'Random',
+            options : ['Random']
+        }
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,46 @@ window.addEventListener('resize', function() {
 })
 
 modeItems.render();
+
+const mouse = {
+    x : null,
+    y : null 
+}
+
+class Color {
+    random() {
+        let color = `rgb(${this.randomVal()},${this.randomVal()},${this.randomVal()})`;
+        return color;
+    }
+
+    randomVal(){
+        let val = Math.floor((Math.random() * 251));
+        return val;
+    }
+}
+
+let color = new Color();
+
+canvas.addEventListener('mousedown', () => {
+    canvas.addEventListener('mousemove', draw)
+});
+
+canvas.addEventListener('mouseup', delMoveHandler)
+
+function delMoveHandler() {
+    canvas.removeEventListener('mousemove', draw)
+}
+
+function draw(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+}
+
+function drawCircle() {
+    ctx.fillStyle = `${color.random()}`;
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    ctx.fill()
+}
+
