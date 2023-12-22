2bbf6b0657e4c0140618f7bd2f2b553de9a42ed1
Artem Nazarchenko <tiymanazar62@gmail.com>
2023-12-22 15:04:52 +0200

    Slightly changed class Music

--- src/js/utils/music.js
+++ src/js/utils/music.js
@@ -3,24 +3,17 @@ class Music {
     play() {
         window.onmousedown = function(){
             let audio = new Audio('audio/FrankOcean-Pink_White.mp3');
-            console.log(audio.nodeType);
             let initialVolume = 0.1;
             audio.volume = initialVolume;
-            console.log(audio)
+            audio.autoplay = true;
 
             let volumeUp = setInterval(()=>{
                 audio.volume += 0.1;
-                console.log(audio.volume);
-
                 if (audio.volume > 0.8) {
                     clearInterval(volumeUp);
-                    console.log(audio.volume);
                 }
-            },1000)
+            }, 1500);
 
-            audio.autoplay = true;
-
-            window.onmousedown = null;
             window.onblur = function(){
                 audio.volume = audio.volume / 2;
                 window.onfocus = function(){
@@ -28,6 +21,11 @@ class Music {
                 }
             }
         }
+        
+        window.onmouseup = function() {
+            window.onmousedown = null;
+        }
+
     }
 }
 
